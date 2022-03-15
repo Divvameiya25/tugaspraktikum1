6cd207a5c84c4a32bfbc4ba86e862393d980aedb
divvameiya586@gmail.com <divvameiya586@gmail.com>
2022-03-15 22:30:49 +0700

    tugaspraktikum1

---
+++ src/page/home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default class Home extends React.Component {
+    render() {
+        return (
+            <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="https://drive.google.com/uc?id=1KjVLBlB0TThBdF0beSHyrYv53-tMcfEWV" class="d-block w-100" alt="1" />
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
